068ce43bd50f61dbc6d5fe4a7b864c9e53a6f6be
ajarek <ajarek@poczta.onet.pl>
2026-07-28 22:32:49 +0200

    feat: add dynamic product details page and steak data configuration

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,14 +1,23 @@
-import { steaks } from '@/public/data/steaks'
-import { CirclePlay } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+"use client"
+import { steaks } from "@/public/data/steaks"
+import { CirclePlay, ShieldCheck, Truck } from "lucide-react"
+import Image from "next/image"
+import React, { use, useState } from "react"
 
-
-const ProductDetailsPage = async ({params}: {params: Promise<{id: string}>}) => {
-  const { id } = await params
+const ProductDetailsPage = ({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) => {
+  const [weight, setWeight] = useState<number>(1)
+  const { id } = use(params)
   const steak = steaks.find((s) => s.id === Number(id))
   if (!steak) {
-    return <div className='min-h-screen flex flex-col justify-center items-center bg-chart-1 text-xl text-black'>Stek nie znaleziony</div>
+    return (
+      <div className='min-h-screen flex flex-col justify-center items-center bg-chart-1 text-xl text-black'>
+        Stek nie znaleziony
+      </div>
+    )
   }
   return (
     <section className='min-h-screen  flex-col bg-foreground p-8 grid grid-cols-1 lg:grid-cols-12 gap-12 items-start py-20'>
@@ -60,7 +69,7 @@ const ProductDetailsPage = async ({params}: {params: Promise<{id: string}>}) => 
               height={200}
             />
             <div className='absolute inset-0 flex items-center justify-center'>
-              <CirclePlay className='size-12 text-background'/>
+              <CirclePlay className='size-12 text-background' />
             </div>
           </div>
         </div>
@@ -68,85 +77,86 @@ const ProductDetailsPage = async ({params}: {params: Promise<{id: string}>}) => 
 
       <div className='lg:col-span-5 flex flex-col gap-8 sticky top-32'>
         <div className='space-y-2'>
-          <p className='font-label-sm text-label-sm uppercase text-secondary tracking-[0.2em]'>
-            ANTRYKOT SEZONOWANY
+          <p className='font-normal text-lg uppercase text-chart-1 tracking-widest'>
+            {steak.tag}
           </p>
-          <h1 className='font-display-lg text-display-lg'>
-            Stek Ribeye Premium
+          <h1 className='font-semibold text-4xl text-background'>
+            {steak.title}
           </h1>
           <div className='flex items-center gap-4 mt-4'>
-            <span className='font-headline-md text-headline-md text-primary'>
-              od 189,00 zł
+            <span className='font-normal text-2xl text-chart-1'>
+              {steak.price} {`PLN`}
             </span>
-            <span className='font-label-sm text-label-sm text-on-surface-variant line-through uppercase'>
-              229,00 zł
+            <span className='font-normal text-lg text-chart-1 line-through'>
+              {(Number(steak.price) * 1.15).toFixed(2)} {`PLN`}
             </span>
           </div>
         </div>
-        <p className='font-body-lg text-body-lg text-on-surface-variant'>
-          Nasz flagowy Antrykot pochodzący z wyselekcjonowanych stad z regionu
-          Podlasia. Sezonowany na sucho przez minimum 28 dni w kontrolowanej
-          temperaturze, co nadaje mu głęboki, orzechowy aromat i niespotykaną
-          kruchość.
-        </p>
+        <p className='font-normal text-base text-chart-1'>{steak.desc}</p>
 
         <div className='space-y-6'>
           <div>
-            <label className='font-label-sm text-label-sm uppercase text-on-surface-variant mb-3 block'>
+            <label className='font-normal text-base uppercase text-chart-1 mb-3 block'>
               Wybierz Wagę
             </label>
             <div className='grid grid-cols-3 gap-2'>
-              <button className='border border-primary bg-primary-container/10 text-primary py-3 font-label-sm text-label-sm uppercase transition-all'>
+              <button
+                className='border border-white text-background hover:border-primary py-3 font-label-sm text-label-sm uppercase transition-all focus:border-primary cursor-pointer'
+                onClick={() => {
+                  setWeight(0.3)
+                }}
+              >
                 300g
               </button>
-              <button className='border border-white text-background hover:border-primary py-3 font-label-sm text-label-sm uppercase transition-all'>
+              <button
+                className='border border-white text-background hover:border-primary py-3 font-label-sm text-label-sm uppercase transition-all focus:border-primary cursor-pointer'
+                onClick={() => {
+                  setWeight(0.5)
+                }}
+              >
                 500g
               </button>
-              <button className='border border-white text-background hover:border-primary py-3 font-label-sm text-label-sm uppercase transition-all'>
+              <button
+                className='border border-white text-background hover:border-primary py-3 font-label-sm text-label-sm uppercase transition-all focus:border-primary cursor-pointer'
+                onClick={() => {
+                  setWeight(1)
+                }}
+              >
                 1kg
               </button>
             </div>
           </div>
           <div className='grid grid-cols-2 gap-4'>
-            <div className='p-4 bg-surface-container border border-white text-background'>
-              <p className='font-label-sm text-label-sm text-on-surface-variant uppercase'>
+            <div className='p-4  border border-white text-background'>
+              <p className=' text-base text-chart-1 uppercase'>
                 Marmurkowatość
               </p>
-              <p className='font-body-lg text-body-lg text-on-surface mt-1'>
-                BMS 7-9
-              </p>
+              <p className=' mt-1'>{steak.marbling}</p>
             </div>
-            <div className='p-4 bg-surface-container border border-white text-background'>
-              <p className='font-label-sm text-label-sm text-on-surface-variant uppercase'>
-                Pochodzenie
-              </p>
-              <p className='font-body-lg text-body-lg text-on-surface mt-1'>
-                Podlasie
-              </p>
+            <div className='p-4  border border-white text-background'>
+              <p className=' text-base text-chart-1 uppercase'>Pochodzenie</p>
+              <p className=' mt-1'>{steak.lineage}</p>
             </div>
           </div>
         </div>
         <div className='flex flex-col gap-4'>
-          <button className='w-full bg-primary-container text-white py-5 font-label-sm text-label-sm uppercase font-bold tracking-widest active:scale-[0.98] transition-all hover:brightness-110'>
-            Dodaj do koszyka — 189,00 zł
+          <button className='w-full bg-primary text-background py-5 font-bold uppercase  tracking-widest active:scale-[0.98] transition-all hover:brightness-110 cursor-pointer'>
+            Dodaj do koszyka — {(Number(steak.price) * weight).toFixed(2)}{" "}
+            {`PLN`}
           </button>
-          <button className='w-full border border-white text-background text-on-surface py-5 font-label-sm text-label-sm uppercase font-bold tracking-widest hover:bg-surface-container-high transition-all'>
+          <button className='w-full border bg-foreground/30  border-background text-background py-5 font-bold uppercase tracking-widest hover:bg-foreground/10 transition-all active:scale-[0.98] cursor-pointer'>
             Kup teraz z dostawą jutro
           </button>
         </div>
         <div className='flex items-center gap-6 pt-4 border-t border-white text-background'>
           <div className='flex items-center gap-2'>
-            <span className='material-symbols-outlined text-primary text-xl'>
-              verified_user
-            </span>
+            <ShieldCheck />
             <span className='font-label-sm text-[10px] uppercase text-on-surface-variant'>
               Certyfikat Premium
             </span>
           </div>
           <div className='flex items-center gap-2'>
-            <span className='material-symbols-outlined text-primary text-xl'>
-              local_shipping
-            </span>
+            <Truck />
             <span className='font-label-sm text-[10px] uppercase text-on-surface-variant'>
               Chłodniczy Transport
             </span>
